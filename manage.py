0d11ce376e8fb7f96808a940a2a3ad857337c41f
Kishor  kumar Sahoo <kishorsahoo7894@gmail.com>
2025-10-29 21:24:05 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,20 +3,24 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todolist.settings')
-    try:
+    
+    # Check if we're in production (Render) and set the port to the dynamic $PORT
+    if 'RUN_MAIN' in os.environ and os.environ.get('RUN_MAIN') == 'true': 
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
+        execute_from_command_line([sys.argv[0], 'runserver', '0.0.0.0:' + os.environ.get('PORT', '8000')])
+    else:
+        try:
+            from django.core.management import execute_from_command_line
+        except ImportError as exc:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            ) from exc
+        execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
